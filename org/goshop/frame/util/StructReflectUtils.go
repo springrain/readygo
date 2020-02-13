739341969e77bb958471254f8f80070a27cd4078
springrain <chunanyong@163.com>
2020-02-13 23:40:03 +0800

    增加字段缓存

--- org/goshop/frame/util/StructReflectUtils.go
+++ org/goshop/frame/util/StructReflectUtils.go
@@ -6,6 +6,15 @@ import (
 	"reflect"
 )
 
+const (
+	exPortPrefix = "_exPortStructFields_"
+
+	privatePrefix = "_privateStructFields_"
+)
+
+// 用于缓存反射的信息
+var cacheStructFieldMap = map[string][]reflect.StructField{}
+
 //获取StructField的信息.只对struct或者*struct判断,如果是指针,返回指针下实际的struct类型.
 //第一个返回值是可以输出的字段(首字母大写),第二个是不能输出的字段(首字母小写)
 func StructFieldInfo(s interface{}) ([]reflect.StructField, []reflect.StructField, error) {
@@ -30,10 +39,22 @@ func StructFieldInfo(s interface{}) ([]reflect.StructField, []reflect.StructFiel
 		}
 	}
 
-	fieldNum := typeOf.NumField()
+	//缓存字段信息
+	exPortCacheKey := exPortPrefix + typeOf.String()
+	privateCacheKey := privatePrefix + typeOf.String()
+	exPortStructFields := cacheStructFieldMap[exPortCacheKey]
+	privateStructFields := cacheStructFieldMap[privateCacheKey]
+	if len(privateStructFields) > 0 || len(privateStructFields) > 0 {
+		return exPortStructFields, privateStructFields, nil
+	}
 
-	exPortStructFields := make([]reflect.StructField, 0)
-	privateStructFields := make([]reflect.StructField, 0)
+	fieldNum := typeOf.NumField()
+	if fieldNum < 1 {
+		return nil, nil, errors.New("entity没有属性")
+	}
+
+	exPortStructFields = make([]reflect.StructField, 0)
+	privateStructFields = make([]reflect.StructField, 0)
 	for i := 0; i < fieldNum; i++ {
 		field := typeOf.Field(i)
 		if ast.IsExported(field.Name) { //如果是可以输出的
@@ -42,5 +63,8 @@ func StructFieldInfo(s interface{}) ([]reflect.StructField, []reflect.StructFiel
 			privateStructFields = append(privateStructFields, field)
 		}
 	}
+
+	cacheStructFieldMap[exPortCacheKey] = exPortStructFields
+	cacheStructFieldMap[privateCacheKey] = privateStructFields
 	return exPortStructFields, privateStructFields, nil
 }
