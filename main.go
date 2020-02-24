fa6644687766e2d7fc1ede8f17838495b02b00f7
springrain <chunanyong@163.com>
2020-02-24 08:34:09 +0800

    修改包名

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	ginconfig "goshop/org/springrain/ginConfig"
+	"goshop/org/springrain/ginext"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,11 +13,11 @@ func main() {
 	// Global middleware
 	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
 	// By default gin.DefaultWriter = os.Stdout
-	r.Use(ginconfig.GinLogger())
+	r.Use(ginext.GinLogger())
 	//r.Use(gin.Logger())
 
 	// Recovery middleware recovers from any panics and writes a 500 if there was one.
-	r.Use(ginconfig.GinRecovery())
+	r.Use(ginext.GinRecovery())
 	//r.Use(gin.Recovery())
 
 	r.GET("/ping", func(c *gin.Context) {
---
+++ org/springrain/ginext/GinLogger.go
@@ -0,0 +1,47 @@
+package ginext
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"goshop/org/springrain/logger"
+)
+
+func GinLogger() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		// Start timer
+		start := time.Now()
+		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
+		// Process request
+		c.Next()
+
+		// Log only when path is not being skipped
+
+		//Request := c.Request
+		//isTerm := isTerm
+		//Keys := c.Keys
+		logFields := []logger.LogField{}
+		// Stop timer
+		logFields = append(logFields, logger.Time("start", start))
+		TimeStamp := time.Now()
+		logFields = append(logFields, logger.Duration("TimeStamp", TimeStamp.Sub(start)))
+		logFields = append(logFields, logger.String("ClientIP", c.ClientIP()))
+		logFields = append(logFields, logger.String("Method", c.Request.Method))
+		logFields = append(logFields, logger.Int("StatusCode", c.Writer.Status()))
+		logFields = append(logFields, logger.String("ErrorMessage", c.Errors.ByType(gin.ErrorTypePrivate).String()))
+		logFields = append(logFields, logger.Int("BodySize", c.Writer.Size()))
+
+		if raw != "" {
+			path = path + "?" + raw
+		}
+		logFields = append(logFields, logger.String("path", path))
+
+		//记录日志
+		logger.Info("ginlog", logFields...)
+
+	}
+}
