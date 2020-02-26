e6eda377d615f126f8a8bfb9214323d4104f0137
springrain <chunanyong@163.com>
2020-02-26 16:11:46 +0800

    提交缓存接口

--- cache/ICacheManager.go
+++ cache/ICacheManager.go
@@ -1,13 +1,14 @@
 package cache
 
 //缓存管理器接口.缓存的结构是map[cacheName string]map[key string]value interface{}
-type ICacheManager interface {
+//内部接口,方便直接使用方法
+type icacheManage interface {
 	//从cache中获取key的值
-	GetFromCache(cacheName string, key string) (interface{}, error)
+	getFromCache(cachName string, key string) (interface{}, error)
 	//设置指定cache中的key值
-	PutToCache(cacheName string, key string, value interface{}) error
+	putToCach(cacheName string, key string, value interface{}) error
 	//清理cache
-	ClearCache(cacheName string) error
+	clearCache(cacheame string) error
 	//失效一个cache中的key
-	EvictKey(cacheName string, key string) error
+	evictKey(cacheName string, key string) error
 }
--- cache/MemeryCacheManager.go
+++ cache/MemeryCacheManager.go
@@ -2,35 +2,35 @@ package cache
 
 import "sync"
 
+//缓存管理器接口.缓存的结构是map[cacheName string]map[key string]value interface{}
 // 用于缓存反射的信息,sync.Map内部处理了并发锁.
 var memeryCacheMap sync.Map
 
 //内存的缓存管理器
-type MemeryCacheManager struct {
+type memeryCacheManager struct {
 }
 
-//创建内存管理器
-func NewMemeryCacheManager() *MemeryCacheManager {
-
+//创建内存管理器,需要给CacheManager中的cacheManager变量赋值
+func NewMemeryCacheManager() error {
 	return nil
 }
 
 //从cache中获取key的值
-func (cacheManager *MemeryCacheManager) GetFromCache(cacheName string, key string) (interface{}, error) {
+func (cacheManager *memeryCacheManager) getFromCache(cacheName string, key string) (interface{}, error) {
 	return nil, nil
 }
 
 //设置指定cache中的key值
-func (cacheManager *MemeryCacheManager) PutToCache(cacheName string, key string, value interface{}) error {
+func (cacheManager *memeryCacheManager) putToCache(cacheName string, key string, value interface{}) error {
 	return nil
 }
 
 //清理cache
-func (cacheManager *MemeryCacheManager) ClearCache(cacheName string) error {
+func (cacheManager *memeryCacheManager) clearCache(cacheName string) error {
 	return nil
 }
 
 //失效一个cache中的key
-func (cacheManager *MemeryCacheManager) EvictKey(cacheName string, key string) error {
+func (cacheManager *memeryCacheManager) evictKey(cacheName string, key string) error {
 	return nil
 }
