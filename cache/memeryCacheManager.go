e232222bc90f9eaf1e304e1e28f9a6c204a09b0f
springrain <chunanyong@163.com>
2020-03-01 11:05:47 +0800

    增加map的处理方法

--- cache/memeryCacheManager.go
+++ cache/memeryCacheManager.go
@@ -1,20 +1,21 @@
 package cache
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 //memeryCacheManager 内存的缓存管理器.缓存的结构是map[cacheName string]map[key stringvalue interface{}
 type memeryCacheManager struct {
 	// 用于缓存反射的信息,sync.Map内部处理了并发锁.用指针地址.
 	memeryCacheMap *sync.Map
-	//cache的Map,用于实际存储map缓存.用指针地址.
-	cacheMap *map[string]interface{}
 }
 
 //NewMemeryCacheManager 创建内存管理器,需要给CacheManger中的cacheManager变量赋值
 func NewMemeryCacheManager() error {
 	newMemeryCacheManager := memeryCacheManager{}
-	newmap := make(map[string]interface{})
-	newMemeryCacheManager.cacheMap = &newmap
+	//newmap := make(map[string]interface{})
+	//newMemeryCacheManager.cacheMap = &newmap
 	//赋值变量,cacheManager只能初始化一次,后面的会覆盖前面的,作为缓存实现
 	cacheManager = &newMemeryCacheManager
 	return nil
@@ -35,7 +36,27 @@ func (cacheManager *memeryCacheManager) clearCache(cacheName string) error {
 	return nil
 }
 
-//evictKey 失效一个cache中的ke
+//evictKey 失效一个cache中的key
 func (cacheManager *memeryCacheManager) evictKey(cacheName string, key string) error {
 	return nil
+}
+
+//获取cacheName,这个方法不在接口内,避免直接获取到cache对象
+func (cacheManager *memeryCacheManager) getCache(cacheName string) (*map[string]interface{}, error) {
+	if len(cacheName) < 1 {
+		return nil, errors.New("cacheName为空")
+	}
+	cacheMapInterface, ok := cacheManager.memeryCacheMap.Load(cacheName)
+	if ok { //如果cacheManager中有值
+		cacheMap, mapOK := cacheMapInterface.(*map[string]interface{})
+		if !mapOK { //如果类型转化失败
+			return nil, errors.New("memeryCacheManager中,从memeryCacheMap取值map类型转化失败")
+		}
+		return cacheMap, nil
+	}
+
+	//cacheManager中没值,初始化一个map放进去,返回这个map
+	newmap := make(map[string]interface{})
+	cacheManager.memeryCacheMap.Store(cacheName, &newmap)
+	return &newmap, nil
 }
