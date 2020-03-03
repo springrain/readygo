db3d13852246ba10f373eca0df4d2c3f5ee69e5f
springrain <chunanyong@163.com>
2020-03-04 00:04:52 +0800

    增加方法

--- permission/permservice/UserRoleMenuService.go
+++ permission/permservice/UserRoleMenuService.go
@@ -7,7 +7,10 @@ import (
 	"readygo/zorm"
 )
 
-const userOrgRoleMenuCacheKey string = "userOrgRoleMenuCacheKey"
+const (
+	userOrgRoleMenuCacheKey string = "userOrgRoleMenuCacheKey"
+	qxCacheKey              string = "qxCacheKey"
+)
 
 //FindRoleByUserId 根据用户Id查询用户的角色
 func FindRoleByUserId(session *zorm.Session, userId string) ([]permstruct.RoleStruct, error) {
@@ -33,7 +36,7 @@ func FindRoleByUserId(session *zorm.Session, userId string) ([]permstruct.RoleSt
 	finder.Append(permstruct.UserRoleStructTableName).Append("  re where re.userId=? and re.roleId=r.id and r.active=1 order by r.privateOrg,r.sortno desc", userId)
 
 	//查询列表
-	errQueryList := zorm.QueryStructList(session, finder, roles, nil)
+	errQueryList := zorm.QueryStructList(session, finder, &roles, nil)
 	if errQueryList != nil {
 		return nil, errQueryList
 	}
@@ -45,5 +48,43 @@ func FindRoleByUserId(session *zorm.Session, userId string) ([]permstruct.RoleSt
 	}
 
 	return roles, nil
+
+}
+
+//FindMenuByRoleId 根据角色Id,查询这个角色有权限的菜单
+func FindMenuByRoleId(session *zorm.Session, roleId string) ([]permstruct.MenuStruct, error) {
+
+	if len(roleId) < 1 {
+		return nil, errors.New("roleId的值不能为空")
+	}
+	cacheKey := "FindMenuByRoleId_" + roleId
+	menus := make([]permstruct.MenuStruct, 0)
+
+	//从缓存中取数据
+	errFromCache := cache.GetFromCache(qxCacheKey, cacheKey, &menus)
+	if errFromCache != nil {
+		return nil, errFromCache
+	}
+	if len(menus) > 0 { //缓存中有数据
+		return menus, nil
+	}
+	//查询角色有权限的菜单
+	finder := zorm.NewFinder()
+	finder.Append("SELECT m.* from ").Append(permstruct.MenuStructTableName).Append(" m,")
+	finder.Append(permstruct.RoleMenuStructTableName).Append("  re where re.roleId=? and re.menuId=m.id and m.active=1 order by m.sortno desc ", roleId)
+
+	//查询列表
+	errQueryList := zorm.QueryStructList(session, finder, &menus, nil)
+	if errQueryList != nil {
+		return nil, errQueryList
+	}
+
+	//放入缓存
+	errPutCache := cache.PutToCache(qxCacheKey, cacheKey, &menus)
+	if errPutCache != nil {
+		return nil, errPutCache
+	}
+
+	return menus, nil
 
 }
