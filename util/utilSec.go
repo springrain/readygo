309e6c172456f535194f46abfcb168cff33beea8
springrain <chunanyong@163.com>
2025-09-17 11:39:00 +0800

    FuncGenerateStringID 函数

--- util/utilSec.go
+++ util/utilSec.go
@@ -2,7 +2,12 @@ package util
 
 import (
 	"crypto/md5"
+	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"math/big"
+	"strings"
+	"time"
 )
 
 // GenerateMD5 生成md5字符串
@@ -10,4 +15,28 @@ func GenerateMD5(str string) string {
 	ctx := md5.New()
 	ctx.Write([]byte(str))
 	return hex.EncodeToString(ctx.Sum(nil))
+}
+
+// FuncGenerateStringID 默认生成字符串ID的函数.方便自定义扩展
+// FuncGenerateStringID Function to generate string ID by default. Convenient for custom extension
+var FuncGenerateStringID func() string = generateStringID
+
+// generateStringID 生成主键字符串
+// generateStringID Generate primary key string
+func generateStringID() string {
+	// 使用 crypto/rand 真随机9位数
+	randNum, randErr := rand.Int(rand.Reader, big.NewInt(1000000000))
+	if randErr != nil {
+		return ""
+	}
+	// 获取9位数,前置补0,确保9位数
+	rand9 := fmt.Sprintf("%09d", randNum)
+
+	// 获取纳秒 按照 年月日时分秒毫秒微秒纳秒 拼接为长度23位的字符串
+	pk := time.Now().Format("2006.01.02.15.04.05.000000000")
+	pk = strings.ReplaceAll(pk, ".", "")
+
+	// 23位字符串+9位随机数=32位字符串,这样的好处就是可以使用ID进行排序
+	pk = pk + rand9
+	return pk
 }
